f7731c461638ff9d936aff346147edf880d27f03
JiajunJasonLi <jiajunlishenzhong@outlook.com>
2023-08-16 16:23:29 -0400

    Election feature

--- contracts/PetShop.sol
+++ contracts/PetShop.sol
@@ -11,40 +11,71 @@ contract PetShop {
         uint256 age;
         string img;
         bool adopted;
-        bool election_winner;
         uint256 num_of_vote;
     }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    mapping(address => bool) public regList;
 
     Pet[] public pets;
     address[] public adopters;
 
     uint public pet_count = 0;
 
+    event vote_event(uint256 indexed _candidateId);
+
+    event register_event(uint256 indexed pet_count);
+
     constructor() public {
-        add_pet('Frieda', 'Corgi', 3, 'images/corgi.jpg');
-        add_pet('Gina', 'Bedlington', 3, 'images/bedlington.jpg');
-        add_pet('Collins', 'German Shephard', 2, 'images/german_shepherd.jpg');
-        add_pet('Melissa', 'Chihuahua', 2, 'images/chihuahua.jpg');
-        add_pet('Jeanine', 'Bichon', 2, 'images/bichon.jpg');
-        add_pet('Elvia', 'Yorkshire', 3, 'images/yorkshire.jpg');
-        add_pet('Latisha', 'Golden Retriever', 3, 'images/golden_retriever.jpg');
-        add_pet('Coleman', 'Husky', 3, 'images/husky.jpg');
-        add_pet('Nichole', 'Maltese', 2, 'images/maltese.jpg');
-        add_pet('Fran', 'Samoyed', 5, 'images/samoyed.jpg');
-        add_pet('Leonor', 'Labrador Retriever', 1, 'images/labrador_retriever.jpg');
-        add_pet('Dean', 'Sheltie', 4, 'images/sheltie.jpg');
-        add_pet('Stevenson', 'Corgi', 7, 'images/corgi.jpg');
-        add_pet('Kristina', 'Golden Retriever', 4, 'images/golden_retriever.jpg');
-        add_pet('Ethel', 'Maltese', 2, 'images/maltese.jpg');
-        add_pet('Terry', 'Pomeranian', 2, 'images/pomeranian.jpg');
+        add_pet('Frieda', 'Corgi', 3, 'images/Corgi.jpg');
+        add_pet('Gina', 'Bedlington', 3, 'images/Bedlington.jpg');
+        add_pet('Collins', 'German Shephard', 2, 'images/German Shepherd.jpg');
+        add_pet('Melissa', 'Chihuahua', 2, 'images/Chihuahua.jpg');
+        add_pet('Jeanine', 'Bichon', 2, 'images/Bichon.jpg');
+        add_pet('Elvia', 'Yorkshire', 3, 'images/Yorkshire.jpg');
+        add_pet('Latisha', 'Golden Retriever', 3, 'images/Golden Retriever.jpg');
+        add_pet('Coleman', 'Husky', 3, 'images/Husky.jpg');
+        add_pet('Nichole', 'Maltese', 2, 'images/Maltese.jpg');
+        add_pet('Fran', 'Samoyed', 5, 'images/Samoyed.jpg');
+        add_pet('Leonor', 'Labrador Retriever', 1, 'images/Labrador Retriever.jpg');
+        add_pet('Dean', 'Sheltie', 4, 'images/Sheltie.jpg');
+        add_pet('Stevenson', 'Corgi', 7, 'images/Corgi.jpg');
+        add_pet('Kristina', 'Golden Retriever', 4, 'images/Golden Retriever.jpg');
+        add_pet('Ethel', 'Maltese', 2, 'images/Maltese.jpg');
+        add_pet('Terry', 'Pomeranian', 2, 'images/Pomeranian.jpg');
     }
 
     // Use memory or direct variables?
     function add_pet(string memory _name, string memory _breed, uint256 age, string memory _img) private {
         uint256 id = pet_count;
         // Adding new pet for setting up the status of each pet
-        pets.push(Pet(id, _name, _breed, age, _img, false, false, 0));
+        pets.push(Pet(id, _name, _breed, age, _img, false, 0));
         pet_count++;
+    }
+
+    function register(string memory _name, string memory _breed, uint256 age, string memory _img) public {
+        add_pet(_name, _breed, age, _img);
+
+        emit register_event(pet_count);
+    }
+
+    function vote(uint256 _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= pet_count);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        pets[_candidateId].num_of_vote++;
+
+        // trigger voted event
+        emit vote_event(_candidateId);
     }
 
     // function adopt(uint petId) public returns (uint) {
---
+++ src/js/election.js
@@ -0,0 +1,150 @@
+electionApp = {
+    web3Provider: null,
+    contracts: {},
+    account: '0x0',
+    hasVoted: false,
+
+    init: async function() {
+        return await electionApp.initWeb3();
+    },
+
+    initWeb3: async function() {
+        // Tryout new method of setting up web3 provider
+        if (window.ethereum) {
+            web3 = new Web3(web3.currentProvider);
+
+            try {
+                //Request account access
+                await window.ethereum.request({ method: "eth_requestAccounts" });
+            } catch (error) {
+                console.error("User denied account access");
+            }
+            electionApp.web3Provider = web3.currentProvider;
+            console.log("modern dapp browser");
+        } else if (window.web3) {
+            electionApp.web3Provider = window.web3.currentProvider;
+            console.log("legacy dapp browser");
+        } else {
+            // If no injected web3 instance is detected, fall back to Ganache
+            electionApp.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+        }
+          
+        web3 = new Web3(electionApp.web3Provider);
+        return electionApp.initContract();
+    },
+
+    initContract: function() {
+        $.getJSON("../PetShop.json", function(petShop) {
+            // Get the necessary contract artifact file and instantiate it with truffle-contract
+            electionApp.contracts.PetShop = TruffleContract(petShop);
+    
+            // Set the provider for our contract
+            electionApp.contracts.PetShop.setProvider(electionApp.web3Provider);
+
+            // Listen for event
+            electionApp.listenForEvents();
+            return electionApp.render();
+
+        });
+    },
+
+    listenForEvents: function() {
+        electionApp.contracts.PetShop.deployed().then(function (instance) {
+            instance.vote_event({
+                fromBlock: 0,
+                toBlock: 'latest'
+            }).watch(function (error, event) {
+                console.log("vote triggered", event);
+                electionApp.render();
+            })
+        })
+    },
+
+    render: function() {
+        var electionInstance;
+
+        web3.eth.getCoinbase(function (err, account) {
+            if (err === null) {
+                electionApp.account = account;
+                $("#accountAddress").html("Your Account: " + account);
+                web3.eth.getBalance(account, function (err, balance) {
+                    if (err === null) {
+                        $("#accountBalance").html("Your Remaining Balance: " + web3.fromWei(balance, "Ether") + " ETH");
+                    }
+                })
+            }
+        })
+
+        electionApp.contracts.PetShop.deployed().then(function (instance) {
+            electionInstance = instance;
+            return electionInstance.pet_count();
+        }).then(function (pet_count) {
+            var pet_array = [];
+            for (var i = 0; i < pet_count; i++) {
+                pet_array.push(electionInstance.pets(i));
+            }
+            Promise.all(pet_array).then(function (values) {
+                var candidatesResults = $("#candidatesResults");
+                candidatesResults.empty();
+
+                var candidatesSelect = $('#candidatesSelect');
+                candidatesSelect.empty();
+                candidatesSelect.append("<option value=\"select\">Choose...</option>");
+                for (var i = 0; i < pet_count; i++) {
+                    var id = values[i][0]['c'][0];
+                    var name = values[i][1];
+                    var breed = values[i][2];
+                    var age = values[i][3];
+                    var img = values[i][4].replace(' ', '%20');
+                    var adopted = values[i][5];
+                    var num_of_vote = values[i][6];
+
+                    if (adopted == false) {
+                        var candidateTemplete = "<tr><th scope=\"row\">" + id + "</th><td>" + name + "</td><td>" + breed + "</td><td>" + age + "</td><td><img src=..\/" + img + " class=\"img-rounded img-center\" border=3 height=100 width=100></img></td><td>" + num_of_vote + "</td></tr>";
+                        candidatesResults.append(candidateTemplete);
+
+                        var candidateOption = "<option value='" + id + "' >" + name + "</ option>";
+                        candidatesSelect.append(candidateOption);
+                    }
+                }
+            });
+            return electionInstance.voters(electionApp.account);
+        }).then(function (hasVoted) {
+            if (hasVoted) {
+                $('form#voteForm').hide();
+            } else {
+                $('form#voteForm').show();
+            }
+            return electionInstance.regList(electionApp.account);
+        }).then(function () {
+            console.log('test');
+        }).catch(function (error) {
+            console.warn(error);
+        });
+    
+    },
+
+    castVote: function () {
+        var candidateId = $('#candidatesSelect').val();
+        console.log(candidateId)
+
+        if(candidateId == 'Select') {
+            window.alert('Please select the candidate');
+        } else {
+            electionApp.contracts.PetShop.deployed().then(function (instance) {
+                return instance.vote(candidateId, { from: electionApp.account });
+            }).then(function (result) {
+                window.alert('Vote Success');
+                // window.location.reload();
+            }).catch(function (error) {
+                console.error(error);
+            })
+        }
+    }
+}
+
+$(function () {
+    $(window).on('load', function () {
+        electionApp.init();
+    });
+});
